3669b6eeefc158bfb03efcf00c7de0fbffb1d46a
Joona Piirainen <joona.piirainen@gmail.com>
2021-03-08 17:56:36 +0200

    snippet data types

--- affaj/src/Lib/Core/Snippet.hs
+++ affaj/src/Lib/Core/Snippet.hs
@@ -1,1 +1,24 @@
-module Lib.Core.Snippet where
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Lib.Core.Snippet
+  ( Snippet (..),
+  )
+where
+
+import Lib.Core.Id (Id)
+import Lib.Core.SnippetContent (SnippetContent)
+import Lib.Core.SnippetDescription (SnippetDescription)
+import Lib.Core.SnippetTitle (SnippetTitle)
+import Lib.Core.User (User)
+
+-- | Data type representing row in the @snippets@ table.
+data Snippet = Snippet
+  { snippetId :: !(Id Snippet),
+    title :: !SnippetTitle,
+    author :: !(Id User),
+    description :: !SnippetDescription,
+    content :: !SnippetContent
+  }
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (FromRow)
+  deriving (FromJSON, ToJSON)
